a0fea447f11210227f45c569eb59b340ce4a7307
jinpx <hujb1983@gmail.com>
2020-08-04 15:51:57 +0800

    thomas
    0804

---
+++ tcp/mysocket.py
@@ -0,0 +1,41 @@
+import socket
+import time
+
+class MySocket : 
+    def __init__(self, sock = None) :
+        if sock is None :
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else :
+            self.s = sock
+    
+    def connect(self, host, port):
+        self.s.connect((host, port))
+
+    def close(self):
+        self.s.close()
+    
+    def send(self, msg):
+        total_sent = 0
+        while total_sent < MSGLEN :
+            sent = self.s.send(msg[total_sent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            total_sent = total_sent + sent
+
+    def recv(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = self.s.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
+## main run
+_my = MySocket()    
+_my.connect("47.89.41.240", 37025)
+time.sleep(12)
+_my.close()
+print("Close")
